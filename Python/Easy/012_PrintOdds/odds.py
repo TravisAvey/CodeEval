1ba3151c077928accc8e76ae71450672217e2ba1
TravisAvey <avey.travis@gmail.com>
2016-05-18 12:17:09 -0500

    Added Comments

--- Python/Easy/012_PrintOdds/odds.py
+++ Python/Easy/012_PrintOdds/odds.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+'''
+    This method will print everyother number
+    from x to y (not inclusive)
+'''
 def print_odds(x, y):
+    # for each number from x -> y, counting by 2
 	for i in range(x, y, 2):
+        # print the number
 		print(i)
 
+# call method to print to stdout 1-99 odds
 print_odds(1, 100)
