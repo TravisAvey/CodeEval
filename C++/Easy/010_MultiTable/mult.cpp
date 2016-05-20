b1e7d1f96df9b1a90b1919cb823782ef9ab85f89
TravisAvey <avey.travis@gmail.com>
2016-05-20 12:49:19 -0500

    Added comments

--- C++/Easy/010_MultiTable/mult.cpp
+++ C++/Easy/010_MultiTable/mult.cpp
@@ -12,16 +12,19 @@ int main()
 
 void PrintTable()
 {
+    // init outer column to 1
     int i=1;
+    // while i is less than 12
     while(i<=12)
     {
-            
+        // loop through 1-12 for each i
         for (int j=1; j<=12; ++j)
-        {
+            // multiply and display the row
             std::cout << std::setw(4) << std::right << i*j;
-        }
+        // increment i
         i++;
-        std::cout << "\n";
+        // output a new line for the next row
+        std::cout << std::endl;
     }
 }
 
