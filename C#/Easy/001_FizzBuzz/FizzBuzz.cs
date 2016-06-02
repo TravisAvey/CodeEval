824801897c9d8222356e993471cf2692928c4cde
TravisAvey <avey.travis@gmail.com>
2016-06-02 09:50:20 -0500

    Done with FizzBuzz, C#.  This used a lot more memory than I was expecting.. need to look at a different way to open and read a file

---
+++ C#/Easy/001_FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class FizzBuzz
+{
+    ///<summary>
+    /// This method prints off numbers 1 through the max
+    /// it will print a F if its divisible by the fizz
+    /// a B if divisible by the buzz param, or FB if divisible
+    /// by both fizz and buzz
+    ///</summary>
+    static void PrintFizzBuzz(int fizz, int buzz, int max)
+    {
+        // loop from 1 to max
+        for (int i=1; i<=max; ++i)
+        {
+            // if divisible by fizz and buzz, print fb
+            if (i % fizz == 0 && i % buzz == 0)
+                Console.Write("FB");
+            // if divisible by fizz, print F
+            else if (i % fizz == 0)
+                Console.Write("F");
+            // if divizible by buzz print B
+            else if (i % buzz == 0)
+                Console.Write("B");
+            // else just print the number
+            else
+                Console.Write(i);
+            // write a space after each
+            Console.Write(" ");
+        }  
+        // write new line
+        Console.WriteLine();  
+    }
+    
+    static void Main(string[] args)
+    {
+       // open file
+       var file = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+       
+       int fizz, buzz, max;
+       string line;
+       // read each line
+       using (var reader = new StreamReader(file))
+       {
+           // while we can get a line
+           while ((line = reader.ReadLine()) != null)
+           {
+               // split line into an array, parse to int
+               List<int> data = new List<int>(Array.ConvertAll(line.Split(' '), int.Parse));
+               // extract the data needed
+               fizz = data[0];
+               buzz = data[1];
+               max = data[2];
+               // print out the fizz buzz
+               PrintFizzBuzz(fizz, buzz, max);
+               
+           }
+       }
+       file.Close();
+    }
+}
