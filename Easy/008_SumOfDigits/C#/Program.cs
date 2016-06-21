47c99b5f3fd748a98e1e7569d08a011c6e250189
TravisAvey <avey.travis@gmail.com>
2016-06-21 12:57:30 -0500

    Added Comments

--- Easy/008_SumOfDigits/C#/Program.cs
+++ Easy/008_SumOfDigits/C#/Program.cs
@@ -8,27 +8,42 @@ class Program
     {
         private int mLength;
 
+        /// <summary>
+        /// Returns the sum of the number passed
+        /// </summary>
         public int Sum(int num)
         {
             return GetSum(num);
         }
 
+        /// <summary>
+        /// setter for the length of the int
+        /// </summary>
         public int Length
         {
             set { mLength = value; }
         }
+
+        /// <summary>
+        /// Private method that calculates the sum of the number 
+        /// </summary>
         private int GetSum(int num)
         {
+            // two variables to hold the answer and each digit (n)
             int ans = 0;
             int n = 0;
 
+            // loop through each digit
             for (int i=1; i<=mLength; ++i)
             {
+                // get the digit on the right
                 n = num % 10;
+                // remove last digit 
                 num /= 10;
+                // sum digit to the answer
                 ans += n;
             }
-            
+            // return the answer
             return ans;
         }
     }
@@ -36,12 +51,17 @@ class Program
 
     public static void Main (string[] args)
     {
+        // init a SumDigits object
         SumDigits sumDigits = new SumDigits();
 
+        // for each line in the file
         foreach (string line in File.ReadLines(args[0]))
         {
+            // convert the current line to an integer
             int num = int.Parse(line);
+            // set the number of digits
             sumDigits.Length = line.Length;
+            // print out the sum of the digits
             Console.WriteLine(sumDigits.Sum(num));
         }
     }
