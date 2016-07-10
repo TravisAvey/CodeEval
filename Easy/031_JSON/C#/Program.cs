0b1ed515dadb28d17f012157bcf275693fd65300
Travis Avey <avey.travis@gmail.com>
2016-07-09 21:48:34 -0500

    Working on the challenge 31

--- Easy/031_JSON/C#/Program.cs
+++ Easy/031_JSON/C#/Program.cs
@@ -1,15 +1,45 @@
 using System;
 using System.IO;
-
+using System.Text.RegularExpressions;
 class Program
 {
+    public class ParseJson
+    {
+        /// <summary>
+        /// Finds the value after the string passed
+        /// </summary>
+        /// <param name="word">The word to parse the JSON for</param>
+        /// <param name="json">The json in a string format</param>
+        public int FindValue (string json, string word)
+        {
+            int val = 0;
+
+            while (true)
+            {
+                // get the current location (at the end of) the word
+                int found = json.IndexOf(word) + word.Length;
+                
+                if (found == 0) break;
+                Console.WriteLine ("found: {0} json {1}", found, json.Length);
+                // set the json to the new location.
+                // the value should be " value ..."
+                json = json.Substring (found);
+
+                
+
+                // val += int.Parse(a);
+            }
+            return val;
+        }
+    }
 
     public static void Main (string[] args)
     {
+        var parse = new ParseJson();
         foreach (var line in File.ReadLines(args[0]))
         {
             if (line == "") continue;
-            System.Console.WriteLine(line);
+            Console.WriteLine (parse.FindValue (line, "Label "));
         }
     }
 }
