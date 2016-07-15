b87028b2b317cbc786a069ddb4e6cc1b4722a634
TravisAvey <avey.travis@gmail.com>
2016-07-15 13:27:27 -0500

    Find first of searching any char in word passed

--- Easy/031_JSON/C++/main.cpp
+++ Easy/031_JSON/C++/main.cpp
@@ -1,13 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstddef>
 
 class ParseJSON
 {
 private:
     std::string mJson;
 
-public:
+    int ExtractNumber (std::string line)
+    {
+        std::string temp;
+        std::cout << "In extract number line is " << line << std::endl;
+        for (int i=0; i<line.size(); ++i)
+        {
+            std::cout << "line[i] : " << line[i] << std::endl;
+            if (isdigit(line[i]))
+            {
+                for (int j=i; j<line.size(); ++j)
+                    temp += line[j];
+            }
+            break;
+        }
+        std::cout << "temp " << temp << std::endl;
+        int num = std::stoi (temp);
+        return num;
+    }
 
+public:
+    int FindValue (std::string word)
+    {
+        int val = 0;
+        std::size_t found = mJson.find_first_of (word);
+        std::cout << "found before loop " << found << std::endl;
+        while (found != std::string::npos)
+        {
+            
+            val += ExtractNumber (mJson.substr (found));
+            
+            found = mJson.find_first_of (word);
+            std::cout << "found in while loop " << found << std::endl;
+        }
+
+        return val;
+    }
+
+    void SetJSON (std::string json)
+    {
+        mJson = json;
+    }
 
 };
 
@@ -18,12 +59,15 @@ int main (int argc, char *argv[])
     if (file)
     {
         std::string line;
-
+        auto parse = new ParseJSON ();
         while (std::getline(file, line))
         {
             if (line == "") continue;
-
-            std::cout << line << std::endl;
+            parse->SetJSON (line);
+            std::cout << parse->FindValue ("Label ") << std::endl;
         }
     }
+
+    file.close (); 
+    return 0;
 }
