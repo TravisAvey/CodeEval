d371a5b671ad0ac4b4c4ca1049d8858443ef0f70
TravisAvey <avey.travis@gmail.com>
2016-08-02 12:49:53 -0500

    Did not read problem correctly
    Need to account for which player has the lowest and return that position.  Also need to change the algorithm to find only the one with the unique lowest, second in sample data should return 0 because no one had a unique number

--- Easy/032_LowestUnique/C++/main.cpp
+++ Easy/032_LowestUnique/C++/main.cpp
@@ -1,22 +1,66 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <sstream>
 #include <map>
-
+#include <climits>
 class LowestUnique
 {
 private:
     std::map<int, int> nums;
-    void InitMap (int n)
+    void InitMap ()
     {
-        
+        nums[1] = 0;
+        nums[2] = 0;
+        nums[3] = 0;
+        nums[4] = 0;
+        nums[5] = 0;
+        nums[6] = 0;
+        nums[7] = 0;
+        nums[8] = 0;
+        nums[9] = 0;
+    }
+
+     void GetNumbers (std::string line)
+    {
+        std::stringstream ss (line);
+        std::string token;
+
+        while (std::getline (ss, token, ' '))
+        {
+            int num = std::stoi (token);
+            int count = nums[num];
+            nums[num] = ++count;
+        }
+    }
+public:
+    LowestUnique (std::string line)
+    {
+        InitMap ();
+        GetNumbers (line);
+    }
+
+    int GetLowest ()
+    {
+        int count = INT_MAX;
+        int lo = 0;
+        for (int i=1; i<=9; ++i)
+        {
+            int num = nums[i];
+            if (num == 0)
+                continue;
+            if (num < count)
+            {
+                lo = i;
+                count = num;
+            }
+        }
+
+        if (lo == INT_MAX)
+            return 0;
+        else
+            return lo;
     }
 };
-
-void GetNumbers (std::vector<int> &, std::string);
-int GetUnique (const std::vector<int> &);
-void InitMap (std::map<int, int> &, int);
 
 int main (int argc, char *argv[])
 {
@@ -29,39 +73,13 @@ int main (int argc, char *argv[])
         std::string line;
         while (std::getline(file, line))
         {
-            std::vector<int> numbers;
-            GetNumbers (numbers, line);
+            auto lowest = new LowestUnique (line);
+            
+            std::cout << lowest->GetLowest () << std::endl;
             
         }
     }
 
     file.close();
     return 0;
-}
-
-void GetNumbers (std::vector<int> &vect, std::string line)
-{
-    std::stringstream ss(line);
-    std::string token;
-
-    while (std::getline (ss, token, ' '))
-        vect.push_back (std::stoi(token));
-}
-
-int GetUnique (const std::vector<int> &vect)
-{
-    int lowest = 10;
-    std::map <int, int> numCount;
-    // loop over each number
-    for (auto i : vect)
-    {
-        // 
-    }
-
-    return lowest;
-}
-
-void InitMap (std::map<int, int> nums, int n)
-{
-
 }
