643551e9c17e52ea652ee893109711705db1de05
TravisAvey <avey.travis@gmail.com>
2016-08-16 13:01:24 -0500

    Working on Longest Lines in C#

---
+++ Moderate/002_LongestLines/C#/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+
+    public static void Main (string[] args)
+    {
+        List<string> lines = new List<string> ();
+
+        foreach (var line in File.ReadLines (args[0]))
+            lines.Add (line);
+
+        int n = int.Parse (lines[0]);
+
+        
+    }
+}
