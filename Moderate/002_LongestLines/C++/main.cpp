a218aac0d552f62c7d4c1e22f8cd8bb4bfc1448e
TravisAvey <avey.travis@gmail.com>
2016-08-16 12:48:45 -0500

    Moderate #2 is done in C++
    Finished with finding the N longest lines in a file.  Used std::sort with a bool operator to sort the data, then print out the last N in the data set

--- Moderate/002_LongestLines/C++/main.cpp
+++ Moderate/002_LongestLines/C++/main.cpp
@@ -1,36 +1,72 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
+// Method declaration to find the longest strings in the file
 void FindLongest (std::vector<std::string> &);
+
+// compare operator that returns a bool based on length of the string
+struct compare
+{
+    // the operator, compares 2 strings
+    bool operator()(const std::string &l, const std::string &r) const
+    {
+        // returns true if left string is less than right
+        return l.size() < r.size();
+    }
+};
 
 int main (int argc, char *argv[])
 {
+    // open the file
     std::fstream file (argv[1], std::ios::in);
 
+    // if the file is opened
     if (file)
     {
+        // declare a vector to hold all the lines
         std::vector<std::string> lines;
+        // declare a string to hold each line
         std::string line;
 
+        // while we can get a line from the file
         while (std::getline (file, line))
+            // add the line to the vector
             lines.push_back (line);
-
+        
+        // call method that will printout the n longest strings
         FindLongest (lines);
     }
+    
+    // close the file
+    file.close ();
 
     return EXIT_SUCCESS;
 }
 
+/*
+    This method prints out the longest n length strings,
+    where n is the first in the vector
+*/
 void FindLongest (std::vector<std::string> &nums)
 {
-    int n = std::stoi(nums[0]);
+    // sort the vector, using the compare struct we made above
+    std::sort (nums.begin (), nums.end (), compare ());
     
-    std::vector<std::string> longest;
-    std::vector<int> lengths;
+    // get the number of strings we need to print out
+    int n = std::stoi (nums[0]);
+    // get the total number of words in the vector
+    int len = nums.size ();
 
-    for (auto s : nums)
+    // while we have n to print out
+    while (n > 0)
     {
-        std::cout << s << std::endl;
+        // decrement len first because we'd be out of range first iteration
+        len--;
+        // output the answer
+        std::cout << nums[len] << std::endl;
+        // decrement the n
+        n--;
     }
 }
