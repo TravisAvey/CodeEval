15fe535309d0556a1742aba512ce2d2d38258faf
TravisAvey <avey.travis@gmail.com>
2016-08-17 09:51:02 -0500

    Done with Longest Lines in C#
    Only got 60/65 for this one, too much memory I think.  Need to look at different data structures to hold the lines in a file.  Possibly IList or IEnumerable?

--- Moderate/002_LongestLines/C#/Program.cs
+++ Moderate/002_LongestLines/C#/Program.cs
@@ -1,20 +1,44 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
+// Only got 60/65 on this one.. a lot of memory, try looking at IList or IEnumerable for lest memory
 class Program
 {
+    /// <summary>
+    /// Prints out the N longest strings, where N is the first in the list
+    /// </summary>
+    private static void Longest (List<string> data)
+    {
+        // snag the N
+        int n = int.Parse (data[0]);
+
+        // Sort the data (this was faster than Linq, but more memory)
+        data.Sort ((x,y) => x.Length - y.Length);
+
+        // get the number of items in the list
+        int i = data.Count;
+        // count down and print out the N number of longest strings
+        while (n > 0)
+        {
+            i--;
+            n--;
+            Console.WriteLine (data[i]);
+        }
+        
+    }
 
     public static void Main (string[] args)
     {
+        // create a list to hold the lines
         List<string> lines = new List<string> ();
 
+        // for each line in the file, add to the list
         foreach (var line in File.ReadLines (args[0]))
             lines.Add (line);
 
-        int n = int.Parse (lines[0]);
-
-        
-        
+        // call method to print out the longest
+        Longest (lines);
     }
 }
