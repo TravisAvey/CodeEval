0125a472538f942bce9ab686935cda7215a214da
Travis Avey <avey.travis@gmail.com>
2017-11-07 19:25:25 -0600

    Added C++ version for compress numbers

--- Easy/043_CompressNumbers/C++/main.cpp
+++ Easy/043_CompressNumbers/C++/main.cpp
@@ -7,13 +7,24 @@ void Compress (std::string &);
 
 int main (int argc, char **argv)
 {
+    // if argc is less than 2, no file passed
+    if (argc < 2)
+        // output message
+        printf("Usage: ./a.out file.txt\n");
+        
+    // grab file user passed to program
     std::fstream file (argv[1], std::ios::in);
 
+    // if the file is good
     if (file)
     {
+        // declare a string to hold each line
         std::string line;
+
+        // while we can get a line from the file
         while (std::getline(file, line))
         {
+            // call method on current line
             Compress (line);
         }
     }
@@ -21,34 +32,48 @@ int main (int argc, char **argv)
     return 0;
 }
 
-void Compress (std::string &line)
-{
-    std::vector<int> nums;
-    std::vector<int> count;
-    std::stringstream ss (line);
-    std::string token;
-    int i = 1;
-    int prevNum = 0;
-
-    while (std::getline (ss, token, ' '))
+/*
+    This method compresses a sequence of numbers
+    4 4 4 5 5 11 11 9
+    as
+    3 4 2 5 2 11 1 9
+*/
+    void Compress (std::string &line)
     {
-        int num = std::stoi (token);
-        // logic fails here: num will change on first time,
-        // therefore the first count will be 1 regardless..
-        if (num != prevNum)
+        // declare a vector to hold the numbers
+        std::vector<int> nums;
+        
+        // initialize a string stream on the line
+        std::stringstream ss (line);
+        // declare a string to hold each token
+        std::string token;
+        
+        // while we can get a token from the string stream
+        while (std::getline (ss, token, ' '))
         {
-            nums.push_back (std::stoi (token));
-            count.push_back (i);
-            i = 1;
+            // parse the token to an int
+            int num = std::stoi (token);
+            // place the int in the number vector
+            nums.push_back(num);
         }
-        i++;
-        prevNum = num;
-    }
 
-    for (auto n : nums)
-        std::cout << n << std::endl;
-    std::cout << "------\n";
-    for (auto c : count)
-        std::cout << c << std::endl;
-    
+        // init a counter to 1
+        int count = 1;
+        // loop through the numbers, but not very last
+        for (int i=0; i<nums.size()-1; i++)
+        {
+            // if the current is the same as the next
+            if (nums[i] == nums[i+1])
+                // increment the counter
+                count++;
+        else
+        {
+            // else, we print out the count of the number
+            printf("%d %d ", count, nums[i]);
+            // re-init counter back to 1
+            count = 1;
+        }
+    }
+   // finally print out the final count number
+   printf("%d %d\n", count, nums[nums.size()-1]); 
 }
