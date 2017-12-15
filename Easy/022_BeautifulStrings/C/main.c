41a8b609d46c11ea3bd1ebbb289338e7efaee745
aveytravis <avey.travis@gmail.com>
2017-12-15 09:22:20 -0600

    Working on Beautiful Strings in C

--- Easy/022_BeautifulStrings/C/main.c
+++ Easy/022_BeautifulStrings/C/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define buf 512
 
-void beauString(char *);
-int lower(char);
+void beauString(char []);
+int compare( const void *a, const void *b);
 
 int main(int argc, char **argv) {
   FILE *file = fopen(argv[1], "r");
@@ -19,28 +20,30 @@ int main(int argc, char **argv) {
   return EXIT_SUCCESS;
 }
 
-void beauString(char *line) {
+void beauString(char line[]) {
   int len = strlen(line);
   char string[len];
+  int j=0;
   int i;
-  int j = 0;
-  int count = 0;
-  for (i=0; i<len; i++ ) {
-    char a = lower(line[i]);
-    if (a) {
-      string[j++] = a;
-      count++;
+  for (i=0;i<len;i++) {
+    if (isalpha(line[i])) {
+      string[j++] = tolower(line[i]);
     }
   }
-
-  for (i=0; i<count; i++)
-    printf("%c", string[i]);
-  printf("\n");
+  
+  int sum = 0;
+  int n = 26;
+  for (i=j-1; i>=0; i--)
+    sum += string[i]*n--;
+  printf("%i\n", sum);
 
 }
 
-int lower(char ch) {
-  if (ch >= 65 && ch <= 90)
-    return ch + 32;
-  return 0;
+int compare (const void *a, const void *b) {
+  int iA = * ( (int *) a);
+  int iB = * ( (int *) b);
+  if (iA == iB) return 0;
+  else if (iA < iB) return -1;
+  else return 1;
+
 }
