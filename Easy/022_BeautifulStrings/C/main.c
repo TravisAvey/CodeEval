2f660943f790643e149bc76faa3e7fe22447ae97
Travis Avey <avey.travis@gmail.com>
2017-12-18 10:22:46 -0600

    Working on beautiful strings.
    Not incrementing the letters number in the numbers array

--- Easy/022_BeautifulStrings/C/main.c
+++ Easy/022_BeautifulStrings/C/main.c
@@ -30,20 +30,37 @@ void beauString(char line[]) {
       string[j++] = tolower(line[i]);
     }
   }
-  
+
+  qsort(string, (size_t)j, (size_t) sizeof(char), compare);
+
+  int k;
+  int numbers[26];
+  for (k=0;k<26;k++) numbers[k] = 0;
+
+  for (k=0;k<j;k++) {
+    char c = string[k];
+    if (isalpha(c)) {
+      int n = numbers[c-'a'];
+      printf("[%c-'a'] = %i \n", c, n);
+      n++;
+      numbers[c-'a'] = n;
+    }
+  }
+
   int sum = 0;
   int n = 26;
-  for (i=j-1; i>=0; i--)
-    sum += string[i]*n--;
+  for (i=25; i>=0; i--) {
+    sum += numbers[i] * n--;
+  }
   printf("%i\n", sum);
 
 }
 
 int compare (const void *a, const void *b) {
-  int iA = * ( (int *) a);
-  int iB = * ( (int *) b);
-  if (iA == iB) return 0;
-  else if (iA < iB) return -1;
-  else return 1;
-
+  // int iA = * ( (int *) a);
+  // int iB = * ( (int *) b);
+  // if (iA == iB) return 0;
+  // else if (iA < iB) return -1;
+  // else return 1;
+  return *(char*) a - *(char*)b;
 }
