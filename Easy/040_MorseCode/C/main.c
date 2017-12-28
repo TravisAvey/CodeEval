42777ec7f851d9025959edc62adc927a2f0421a4
aveytravis <avey.travis@gmail.com>
2017-12-28 12:26:47 -0600

    Started making a hash table for the MoreseCode Challenge
    Should modify the code so it stores a char and not a string, only need to return a letter for each morse code

---
+++ Easy/040_MorseCode/C/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUF 512
+#define SIZE 50
+
+typedef struct {
+  int size;
+  char **keys;
+  char **values;
+} hash_t;
+
+void initHashTable(hash_t *);
+void *newHash(int size);
+int getIndex(hash_t *, char *);
+void insert(hash_t *, char *, char *);
+char *get(hash_t *, char *);
+
+int main(int argc, char **argv) {
+  hash_t *h = newHash(SIZE);  
+  initHashTable(h);
+  
+  return EXIT_SUCCESS;
+}
+
+void initHashTable(hash_t *h) {
+  insert(h, ".-", "A");   insert(h, "-...", "B");
+  insert(h, "-.-.", "C"); insert(h, "-..", "D");
+  insert(h, ".", "E");    insert(h, "..-.", "F");
+  insert(h, "--.", "G");  insert(h, "....", "H");
+  insert(h, "..", "I");    insert(h, ".---", "J");
+  insert(h, "-.-", "K");  insert(h, ".-..", "L");
+  insert(h, "--", "M");   insert(h, "-.", "N");
+  insert(h, "---", "O");  insert(h, ".--.", "p");
+}
+
+void *newHash(int size) {
+  hash_t *h = malloc(sizeof(hash_t));
+  h->keys = malloc(sizeof(char *) * size);
+  h->values = malloc(sizeof(char *) * size);
+  h->size = size;
+  return h;
+}
+
+int getIndex(hash_t *h, char *key) {
+  int i = (int) key % h->size;
+  while (h->keys[i] && h->keys[i] != key)
+    i = (i + 1) % h->size;
+  return i;
+}
+
+
+void insert(hash_t *h, char *key, char *value) {
+
+  int i = getIndex(h, key);
+  h->keys[i] = key;
+  h->values[i] = value;
+}
+
+char *get(hash_t *h, char *key) {
+  int i = getIndex(h, key);
+  return h->values[i];
+}
+
+
