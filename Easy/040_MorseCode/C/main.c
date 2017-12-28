2bbc1acb4c23a6680c437f70253f82affc619f05
aveytravis <avey.travis@gmail.com>
2017-12-28 14:58:21 -0600

    Becoming messy.  Maybe Hash Table not working correctly

--- Easy/040_MorseCode/C/main.c
+++ Easy/040_MorseCode/C/main.c
@@ -16,12 +16,83 @@ void *newHash(int size);
 int getIndex(hash_t *, char *);
 void insert(hash_t *, char *, char);
 char get(hash_t *, char *);
+void morseCode(hash_t *, char *);
+int doubleSpaces(char *, char *[]);
+void output(hash_t *, char *);
 
 int main(int argc, char **argv) {
+  if (argc < 2) return EXIT_FAILURE;
+  FILE *file = fopen(argv[1], "r");
+  char line[BUF];
   hash_t *h = newHash(SIZE);  
   initHashTable(h);
+  while (fgets(line, BUF, file)) {
+    char *token = strtok(line, "\n");
+    morseCode(h, token);
+
+  }
   
   return EXIT_SUCCESS;
+}
+
+void morseCode(hash_t *h, char *line) {
+  char *codes[10];
+  int i = doubleSpaces(line, codes);
+
+  if (i > 0) {
+    int j;
+    for(j=0; j<i; j++) {
+      output(h, codes[j]);
+      printf(" ");
+    }
+  
+  } else {
+    output(h, line);
+  }
+  puts("");
+
+}
+
+void output(hash_t *h, char *code) {
+  printf("*code = %s\n", code);
+  char *token = strtok(code, " ");
+  printf("*token = %s\n", token);
+  while (token) {
+    printf("%c", get(h, token));
+    token = strtok(NULL, " ");
+  }
+}
+
+int doubleSpaces(char *line, char *codes[]) {
+  int len = strlen(line);
+  int pos[10];
+  int j = 0;
+  int i;
+  for(i=0; i<len-1; i++) {
+    char c = line[i];
+    if (c == ' ' && line[i+1] == ' ') {
+      pos[j++] = i;
+    }
+  }
+  int n = 0;
+  if (j>0) {
+    int last = 0;
+    
+    for (i=0; i<j; i++) {
+      int loc = pos[i];
+      char *to = (char *) malloc(sizeof(char) * loc-last+1);
+      strncpy(to, line + last, loc - last);
+      to[loc-last+1] = '\0';
+      last = loc;
+      codes[n++] = to;
+    }
+    // copy last part over
+    char *to = (char *) malloc(sizeof(char) * len-last+1);
+    strncpy(to, line + last, len-last);
+    to[len-last+1] = '\0';
+    codes[n++] = to;
+  }
+  return n;
 }
 
 void initHashTable(hash_t *h) {
