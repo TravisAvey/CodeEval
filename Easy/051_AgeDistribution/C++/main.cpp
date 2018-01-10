10d775d01955e0b4177011578dd2e965d7168f07
Travis Avey <avey.travis@gmail.com>
2018-01-10 09:15:02 -0600

    Added comments
    CodeEval is still processing this solution.  Either using stringstream or stoi to convert the int.  Not sure what could be causing that, possible error on CodeEval's side (server needs restart or something)

--- Easy/051_AgeDistribution/C++/main.cpp
+++ Easy/051_AgeDistribution/C++/main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 void ageDistribution(std::string &);
 
 int main(int argc, char **argv) {
+  // if no file exit
   if (argc < 2) return -1;
+  // open file
   std::fstream file(argv[1], std::ios::in);
+  // if file is opened
   if (file) {
+    // init a string
     std::string line;
+    // get each line in file, store in string
     while (std::getline(file, line)) {
+      // call method for age distribution
       ageDistribution(line);
     }
   }
-
   return 0;
-
 }
 
+// This method outputs a statement to stdout
+// based on the input, string as age
 void ageDistribution(std::string &line) {
-  int age = std::stoi(line);
-  
+  // init a string stream on the line
+  std::stringstream ss(line);
+  // init an integer value to hold age
+  int age = 0;
+  // read from stringstream into the int
+  ss >> age;
+  //int age = std::stoi(line);
+  // Ouput message based on age
   if (age >= 0 && age <= 2)
     std::cout << "Still in Mama's arms" << std::endl;
   else if (age >= 3 && age <= 4)
@@ -36,7 +49,6 @@ void ageDistribution(std::string &line) {
     std::cout << "Working for the man" << std::endl;
   else if (age >= 66 && age <= 100)
     std::cout << "The Golden Years" << std::endl;
-  else
+  else if (age < 0 || age > 100)
     std::cout << "This program is for humans" << std::endl; 
-
 }
